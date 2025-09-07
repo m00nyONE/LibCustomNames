2b1167c06d5ac48954b3f6b281c2aa0cc9aaeef2
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-09-07 13:34:12 +0200

    add/edit/remove users

--- nil.lua
+++ nil.lua
@@ -10,6 +10,7 @@ local function readOnly(t)
         __newindex = function(_, k, v)
             if TBUG or Zgoo then return end -- don't show message edits when Torchbug or Zgoo are running
             d("attempt to update read-only table")
+            --error("<-- This malicious Addon tried to overwrite protected data! Screenshot the Error and report the Addon! Remove it Immediately afterwards!", 2)
         end,
     }
     setmetatable(proxy, metatable)
