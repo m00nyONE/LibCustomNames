aea3f463aef905b780805f49fd8cb9113ab9ba83
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-09-13 14:46:43 +0200

    add/edit/remove users

--- names/misc7.lua
+++ names/misc7.lua
@@ -228,3 +228,4 @@ n["@IgnDarius"] = {"Darius", "|c18f1d1Darius|r"}
 n["@Oshin3"] = {"Oshin", "|c40e0d0Oshin|r"}
 n["@pkstar12"] = {"pkstar12", "|caaf9ebp|r|cb2f2dek|r|cbaecd0s|r|cc2e6c2t|r|ccadfb4a|r|cd2d9a7r|r|cdad2991|r|ce2cc8b2|r"}
 n["@DrewUout1"] = {"DrewUout", "|c0bf6f6D|r|c15daeer|r|c1fbee5e|r|c29a2ddw|r|c3286d4U|r|c3c6acco|r|c464ec3u|r|c5032bbt|r"}
+n["@Azinaaa"] = {"Blink", "|cab00ffB|r|cb700ffl|r|cc200ffi|r|cce00ffn|r|cd900ffk|r"}
