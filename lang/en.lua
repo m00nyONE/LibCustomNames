8698d5f87c6edccebc0d4f5e62858a02341c9310
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-09-30 14:21:15 +0200

    add license headers

--- lang/en.lua
+++ lang/en.lua
@@ -1,3 +1,6 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
 local strings = {
     LCN_MENU = "LibCustomNames",
     LCN_MENU_HEADER = "my custom name",
--- menu.lua
+++ menu.lua
@@ -1,3 +1,6 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
 local lib_name = "LibCustomNames"
 local lib = _G[lib_name]
 local lib_author = lib.author
