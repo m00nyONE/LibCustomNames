478d00637f7121b103e3f716b7630e5c4907e736
Thorben Heemsoth <T.Heemsoth@outlook.com>
2025-10-11 15:29:24 +0200

    - Insert CroNoS.TOG and Update Seolaelis

--- PC/names/cd.lua
+++ PC/names/cd.lua
@@ -5,8 +5,9 @@ local lib_name = "LibCustomNames"
 local lib = _G[lib_name]
 local n = lib.GetNamesTable()
 
+n["@CroNoS.TOG"] = {"Cronos", "|cffe200C|r|cffcb00r|r|cffb500o|r|cff9e00n|r|cff8700o|r|cff7000s|r"}
 n["@Sacriefy"] = {"Luke", "|cf5ca40L|r|ceca780u|r|ce283bfk|r|cd860ffe|r"}
-n["@Seolaelis"] = {"Thorben", "|cffa4dcT|r|cff88cfh|r|cff6dc2o|r|cff52b5r|r|cff37a8b|r|cff1b9be|r|cff008en|r"}
+n["@Seolaelis"] = {"Thorben", "|c21f8dbT|r|c41f9b6h|r|c62fa92o|r|c82fc6dr|r|ca3fd49b|r|cc3fe24e|r|ce4ff00n|r"}
 n["@SirBoost"] = {"SirBoost", "|cf5198dBoost|r"}
 n["@SkarkOo"] = {"SkarkOo", "|c1dfbffS|r|c3bfbffk|r|c58fbffa|r|c75faffr|r|c92faffk|r|cb0faffO|r|ccdfaffo|r"}
 n["@Wolf384"] = {"WolF", "|cf30090W|r|ce700b5o|r|cda00dal|r|cce00ffF|r"}
