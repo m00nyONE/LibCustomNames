0d22005e6ac586a03056f25910bc4f9c9f047c94
Thorben Heemsoth <T.Heemsoth@outlook.com>
2025-10-11 14:21:10 +0200

    - Updated Seolaelis color-gradient

--- PC/names/cd.lua
+++ PC/names/cd.lua
@@ -6,7 +6,7 @@ local lib = _G[lib_name]
 local n = lib.GetNamesTable()
 
 n["@Sacriefy"] = {"Luke", "|cf5ca40L|r|ceca780u|r|ce283bfk|r|cd860ffe|r"}
-n["@Seolaelis"] = {"Thorben", "|cffda24T|r|cffb549h|r|cff916do|r|cff6d92r|r|cff49b6b|r|cff24dbe|r|cff00ffn|r"}
+n["@Seolaelis"] = {"Thorben", "|cffa4dcT|r|cff88cfh|r|cff6dc2o|r|cff52b5r|r|cff37a8b|r|cff1b9be|r|cff008en|r"}
 n["@SirBoost"] = {"SirBoost", "|cf5198dBoost|r"}
 n["@SkarkOo"] = {"SkarkOo", "|c1dfbffS|r|c3bfbffk|r|c58fbffa|r|c75faffr|r|c92faffk|r|cb0faffO|r|ccdfaffo|r"}
 n["@Wolf384"] = {"WolF", "|cf30090W|r|ce700b5o|r|cda00dal|r|cce00ffF|r"}
