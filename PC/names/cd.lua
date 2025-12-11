840da9afc67cd980fcaae8fb060d1fb7ed9c0613
haze068 <florian.068002@gmail.com>
2025-12-11 22:58:50 +0100

    Update cd.lua

--- PC/names/cd.lua
+++ PC/names/cd.lua
@@ -5,6 +5,13 @@ local lib_name = "LibCustomNames"
 local lib = _G[lib_name]
 local n = lib.GetNamesTable()
 
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomNames"
+local lib = _G[lib_name]
+local n = lib.GetNamesTable()
+
 n["@CroNoS.TOG"] = {"Cronos", "|cffe200C|r|cffcb00r|r|cffb500o|r|cff9e00n|r|cff8700o|r|cff7000s|r"}
 n["@NamelessOne87"] = {"Robert", "|cff77ffR|r|cff89ffo|r|cff9bffb|r|cffadffe|r|cffbfffr|r|cffd1fft|r"}
 n["@Sacriefy"] = {"Luke", "|cf5ca40L|r|ceca780u|r|ce283bfk|r|cd860ffe|r"}
@@ -14,3 +21,7 @@ n["@SkarkOo"] = {"SkarkOo", "|c1dfbffS|r|c3bfbffk|r|c58fbffa|r|c75faffr|r|c92faf
 n["@Venator21"] = {"Venator", "|ce94409V|r|cec5113e|r|cf05e1cn|r|cf36c25a|r|cf7792et|r|cfa8638o|r|cfe9341r|r"}
 n["@Wolf384"] = {"WolF", "|cf30090W|r|ce700b5o|r|cda00dal|r|cce00ffF|r"}
 n["@xxFeyLamiaxx"] = {"Fey", "|c8d0714F|r|cb40d27e|r|cdc143by|r"}
+n["@abstand1312"] = {"abstand1312", "|c00f2ffabstand1312|r"}
+n["@Marcosis_Ginger92"] = {"Marco", "|cf800ffM|r|cf600ffa|r|cf400ffr|r|cf200ffc|r|cf000ffo|r"}
+n["@KeyTexTv"] = {"KexTexTv", "|cff0030KexTexTv|r"}
+n["@Saryoo"] = {"Saryoo", "|c200038Saryoo|r"}
