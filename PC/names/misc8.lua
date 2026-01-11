65f09228f30593c063bd8e379864a7d1b8dea1f8
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2026-01-11 11:40:57 +0100

    add/edit/remove users

---
+++ PC/names/misc8.lua
@@ -0,0 +1,8 @@
+-- SPDX-FileCopyrightText: 2026 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomNames"
+local lib = _G[lib_name]
+local n = lib.GetNamesTable()
+
+n["@RedBoul"] = {"RedBoul", "|ca8211eR|r|cb7422fe|r|cc56240d|r|cd48252B|r|ce2a263o|r|cf1c374u|r|cffe385l|r"}
