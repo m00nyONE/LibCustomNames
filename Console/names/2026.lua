948839fc2fe02c14fb81c28bf35691635dddf212
Alex Studley <studleton@gmail.com>
2026-01-11 11:36:08 +0000

    Add console requests (#152)
    * Update console names
    
    * Add 2026 console file and new names
    
    * Add console 2026 file to .addon

---
+++ Console/names/2026.lua
@@ -0,0 +1,11 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomNames"
+local lib = _G[lib_name]
+local n = lib.GetNamesTable()
+
+n["@YippyMippy"] = {"Hateful Shrew", "|c5a2effH|r|c5f37ffa|r|c6440fft|r|c6a49ffe|r|c6f51fff|r|c745affu|r|c7963ffl|r |c8375ffS|r|c897effh|r|c8e86ffr|r|c938fffe|r|c9898ffw|r"}
+n["@RickyMrx"] = {"ll RickyMrx ll", "|c00ffffl|r|c00ebffl|r |c00c4ffR|r|c00b1ffi|r|c009dffc|r|c0089ffk|r|c0076ffy|r|c0062ffM|r|c004effr|r|c003bffx|r |c0014ffl|r|c0000ffl|r"}
+n["@Bini1403"] = {"Bini1403", "|cff00ffB|r|ce502e6i|r|ccb04cdn|r|cb105b4i|r|c97079c1|r|c7c09834|r|c620b6a0|r|c480c513|r"}
+n["@QcTonyDage07"] = {"TonyDage07", "|c00ffffT|r|c1ce3ffo|r|c39c6ffn|r|c55aaffy|r|c718effD|r|c8e71ffa|r|caa55ffg|r|cc639ffe|r|ce31cff0|r|cff00ff7|r"}
