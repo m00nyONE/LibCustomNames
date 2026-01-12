5eb5b297808373503523a4dcb4940fbd909fbfd9
Sam Gray <samleegray@gmail.com>
2026-01-12 15:02:28 -0800

    Update username for @samihaize (#154)

--- PC/names/misc8.lua
+++ PC/names/misc8.lua
@@ -6,3 +6,4 @@ local lib = _G[lib_name]
 local n = lib.GetNamesTable()
 
 n["@RedBoul"] = {"RedBoul", "|ca8211eR|r|cb7422fe|r|cc56240d|r|cd48252B|r|ce2a263o|r|cf1c374u|r|cffe385l|r"}
+n["@samihaize"] = {"samihaize", "|cf500e2s|r|ceb00e5a|r|ce100e9m|r|cd700edi|r|cce00f0h|r|cc400f4a|r|cba00f8i|r|cb000fbz|r|ca600ffe|r"}
