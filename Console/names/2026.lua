2fe45444687f79cf4d7f8645059d58980db23974
MicRaider <dk58b2msb4@privaterelay.appleid.com>
2026-01-14 21:04:14 +0100

    Change Name (#159)

--- Console/names/2026.lua
+++ Console/names/2026.lua
@@ -9,4 +9,4 @@ n["@YippyMippy"] = {"Hateful Shrew", "|c5a2effH|r|c5f37ffa|r|c6440fft|r|c6a49ffe
 n["@RickyMrx"] = {"ll RickyMrx ll", "|c00ffffl|r|c00ebffl|r |c00c4ffR|r|c00b1ffi|r|c009dffc|r|c0089ffk|r|c0076ffy|r|c0062ffM|r|c004effr|r|c003bffx|r |c0014ffl|r|c0000ffl|r"}
 n["@Bini1403"] = {"Bini1403", "|cff00ffB|r|ce502e6i|r|ccb04cdn|r|cb105b4i|r|c97079c1|r|c7c09834|r|c620b6a0|r|c480c513|r"}
 n["@QcTonyDage07"] = {"TonyDage07", "|c00ffffT|r|c1ce3ffo|r|c39c6ffn|r|c55aaffy|r|c718effD|r|c8e71ffa|r|caa55ffg|r|cc639ffe|r|ce31cff0|r|cff00ff7|r"}
-n["@MicRaider82"] = {"MicRaider", "|c00c4ffM|r|c1cb9ffi|r|c39afffc|r|c7199ffR|r|c8e8fffa|r|caa84ffi|r|cc679ffd|r|ce36fffe|r|cff64ffr|r"}
+n["@MicRaider82"] = {"MíçRąídër", "|c00c4ffM|r|c1cb9ffí|r|c39afffç|r|c7199ffR|r|c8e8fffą|r|caa84ffí|r|cc679ffd|r|ce36fffë|r|cff64ffr|r"}
