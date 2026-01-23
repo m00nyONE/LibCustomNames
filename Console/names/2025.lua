ed280b7a9284acf61547a74f9de8ac9ac04f7feb
corvidhypnagog <corvid.hypnagog@gmail.com>
2026-01-23 17:27:03 -0500

    Update 2025.lua

--- Console/names/2025.lua
+++ Console/names/2025.lua
@@ -250,4 +250,4 @@ n["@x13lys13x"] = {"x13lys13x","|c00a1d8x13lys13x|r"}
 n["@manaere"] = {"manaere","|c7600b5manaere|r"}
 n["@OnceUponMidnight"] = {"midnight","|c00ab83midnight|r"}
 n["@Epigania"] = {"Epigania","|cFF0018E|r|cFFA52Cp|r|cFFFF41i|r|c008018g|r|c008018a|r|c0000F9n|r|c86007Di|r|cFF0018a|r"}
-n["@simply_Sabros42"] = {"simply_sabros","|c1e2ea6simply_sabros|r"}
+n["@simply_Sabros42"] = {"simply_sabros","|c00ffffs|r|c00eaffi|r|c00d5ffm|r|c00bfffp|r|c00aaffl|r|c0095ffy|r|c0080ff_|r|c006affs|r|c0055ffa|r|c0040ffb|r|c002bffr|r|c0015ffo|r|c0000ffs|r"}
