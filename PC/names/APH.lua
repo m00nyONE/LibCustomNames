42d21a95652857784702ca305b9b970149bb25e4
APHONIC <missaphonic@gmail.com>
2026-01-31 10:20:29 +0800

    Add files via upload

---
+++ PC/names/APH.lua
@@ -0,0 +1,8 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomNames"
+local lib = _G[lib_name]
+local n = lib.GetNamesTable()
+
+n["@APHONlC"] = {"APHONlC", "|ca500f3A|r|cb400e6P|r|cc300daH|r|cd200cdO|r|ce100c1N|r|cf000b4l|r|cff00a8C|r"}
