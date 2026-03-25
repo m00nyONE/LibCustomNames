dff23deb11b6501cf81928c1d2b23792f1fe48e2
Bob Doolittle <github@doolittle.us.com>
2026-03-24 18:15:08 -0700

    temp

--- Console/names/bobdrad.lua
+++ Console/names/bobdrad.lua
@@ -7,9 +7,10 @@ local n = lib.GetNamesTable()
 
 -- this list is maintained by @BobDRAD on PS-NA
 
+n["@AnchorLady113"] = {"AnchorLady", "|c5e00ffA|r|c6f00ffn|r|c8100ffc|r|c9300ffh|r|ca500ffo|r|cb700ffr|r|cc900ffL|r|cdb00ffa|r|ced00ffd|r|cff00ffy|r"}
 n["@Angels_Slaughter"] = {"Angel (*.*)", "|cBD0404A|r|cC3042An|r|cCA0353g|r|cD1037Ee|r|cD703ADl|r|cDE02DE |r|cB702E4(|r|c8E01EB*|r|c6101F2.|r|c3200F8*|r|c0000FF)|r"}
 n["@BakerBakker"] = {"Baker", "|c0cf3dbB|r|c0cf3dba|r|c0cf3dbk|r|c0cf3dbe|r|c0cf3dbr|r"}
-n["@BobDRAD"] = {"Bob Abides", "|cFA78FFB|r|cB66EFEo|r|c6A65FCb|r|c5CA2FB |r|c53EAF9A|r|c4AF8B6b|r|c41F65Di|r|c73F538d|r|cC9F42Fe|r|cF2BF27s|r"}
+n["@BobDRAD"] = {"the bob abides", "|cFF0088t|r|cFF0012h|r|cFF6300e|r|cFFD900 |r|cAFFF00b|r|c3AFF00o|r|c00FF3Cb|r|c00FFB2 |r|c00D6FFa|r|c0061FFb|r|c1500FFi|r|c8B00FFd|r|cFF00FEe|r|cFF0088s|r"}
 n["@cheezhead84"] = {"Cheez", "|c1c8c07C|r|c4d9f06h|r|c7fb306e|r|cb0c606e|r|ce2da06z|r"}
 n["@famed_taxpayer1"] = {"Famed <3", "|cff00fbF|r|cda21fba|r|cb643fcm|r|c9165fce|r|c6d86fdd|r|c48a8fd |r|c24cafe<|r|c00ecff3|r"}
 n["@ImmolationO_o"] = {"Immolation xD", "|cf10000I|r|cf21300m|r|cf32600m|r|cf43900o|r|cf54d01l|r|cf66001a|r|cf87301t|r|cf98601i|r|cfa9a02o|r|cfbad02n|r|cfcc002 |r|cfdd302x|r|cffe703D|r"}
