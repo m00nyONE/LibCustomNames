907c5f86ed71d25687d9b70d08ecec40a5f92973
blkx777 <rhysgdnr@gmail.com>
2026-06-15 20:07:42 +1000

    Update 2026.lua
    Added custom name FOR BLKx777

--- Console/names/2026.lua
+++ Console/names/2026.lua
@@ -89,4 +89,4 @@ n["@SociaI Wish"] = {"Social Wish", "|cff2bfbS|r|c1fff1do|r|c1dffe8c|r|cff01bci|
 n["@SimplyxMike"] = {"SimplyxMike", "|c0000d3S|r|c0219bei|r|c0332a9m|r|c054c94p|r|c06657fl|r|c087e6ay|r|c0a9754x|r|c0bb03fM|r|c0dca2ai|r|c0ee315k|r|c10fc00e|r"}
 n["@heflin-SA"] = {"Top G", "|c3a87feT|r|c607bfeo|r|c876ffep|r |cd357feG|r"}
 n["@RkindaIeft"] = {"Girlbossing", "|cff00bbG|r|ce600c2i|r|ccc00c9r|r|cb300cfl|r|c9900d6b|r|c8000ddo|r|c6600e4s|r|c4d00ebs|r|c3300f1i|r|c1a00f8n|r|c0000ffg|r"}
-n["@BLKx777"] = {"BLKx777", "|c00F5FFB|r|c338DFFL|r|c7A5CFFK|r|cA35CFFx|r|cD93CFF7|r|c9F6FFF7|r|c00F5FF7|r"}
+n["@BLKx777"] = {"BLKx777", "|c00F5FFB|r|c3BB5FFL|r|c6D7AFFK|r|c9A4EFFx|r|cD93CFF7|r|c8A7BFF7|r|c00E5FF7|r"}
