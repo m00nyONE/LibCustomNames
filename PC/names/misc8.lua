9fe5cf83d8cc279ea0dda545305955b0c9528d8f
JustNormalUser <58004798+JustNormalUser@users.noreply.github.com>
2026-09-02 22:36:44 +0300

    add HondaForLife name

--- PC/names/misc8.lua
+++ PC/names/misc8.lua
@@ -168,3 +168,4 @@ n["@xDeathroar"] = {"Deathroar", "|c296ebeD|r|c407cace|r|c568b9ba|r|c6d9a89t|r|c
 n["@khaleeki"] = {"khaleeki", "|c7aefd1k|r|c8dded5h|r|ca0ced8a|r|cb3bddcl|r|cc6addfe|r|cd99ce2e|r|cec8ce6k|r|cff7be9i|r"}
 n["@ScarGard12"] = {"ScarGard12", "|ce171f6S|r|cd67df2c|r|cca89eda|r|cbf95e9r|r|cb4a2e4G|r|ca9aedfa|r|c9ebadbr|r|c92c6d6d|r|c87d2d21|r|c7cdecd2|r"}
 n["@Banira'Ai"] = {"Banira'Ai", "|cffe6d8B|r|cffded4a|r|cffd5d0n|r|cffcdcci|r|cffc5c7r|r|cffbdc3a|r|cffb4bf'|r|cffacbbA|r|cffa4b7i|r"}
+n["@HondaForLife"] = {"HondaForLife", "|c15eafeH|r|c2bd4feo|r|c40bffen|r|c55aafed|r|c6a95fea|r|c7f7fffF|r|c956affo|r|caa55ffr|r|cbf40ffL|r|cd52affi|r|cea15fff|r|cff00ffe|r"}
